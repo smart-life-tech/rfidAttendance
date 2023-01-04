d60b88b1480727599c2beff48613973b3cb065af
smart-life-tech <christlightworld@gmail.com>
2023-01-04 11:12:41 +0100

    modified:   esp32cam/esp32cam.ino

--- esp32cam/esp32cam.ino
+++ esp32cam/esp32cam.ino
@@ -3,51 +3,71 @@
 #include "soc/soc.h"
 #include "soc/rtc_cntl_reg.h"
 #include "esp_camera.h"
+#include "esp_http_client.h"
+#include "time.h"
+const int capture_interval = 5000;                        // Microseconds between captures
+const char *post_url = "http://website.com/cam/cam1.php"; // Location where images are POSTED
 
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char *open_hour = "11";  // the opening hour
+const char *close_hour = "14"; // the closing hour
 
-String serverName = "http://192.168.1.45";   // REPLACE WITH YOUR computer IP ADDRESS
-//String serverName = "example.com";   // OR REPLACE WITH YOUR DOMAIN NAME
+// select the resolution
+// #define FRAMESIZE FRAMESIZE_QQVGA   // 160x120
+// #define FRAMESIZE FRAMESIZE_QQVGA2  // 128x160
+// #define FRAMESIZE FRAMESIZE_QCIF    // 176x144
+// #define FRAMESIZE FRAMESIZE_HQVGA   // 240x176
+// #define FRAMESIZE FRAMESIZE_QVGA    // 320x240
+// #define FRAMESIZE FRAMESIZE_CIF     // 400x296
+// #define FRAMESIZE FRAMESIZE_VGA     // 640x480
+// #define FRAMESIZE FRAMESIZE_SVGA    // 800x600
+#define FRAMESIZE FRAMESIZE_XGA // 1024x768
 
-String serverPath = "http://192.168.1.45/rfidattendance/upload.php";     // The default serverPath should be upload.php
+const char *ssid = "REPLACE_WITH_YOUR_SSID";
+const char *password = "REPLACE_WITH_YOUR_PASSWORD";
+
+String serverName = "http://192.168.1.45"; // REPLACE WITH YOUR computer IP ADDRESS
+// String serverName = "example.com";   // OR REPLACE WITH YOUR DOMAIN NAME
+
+String serverPath = "http://192.168.1.45/rfidattendance/upload.php"; // The default serverPath should be upload.php
 
 const int serverPort = 80;
 
 WiFiClient client;
 
 // CAMERA_MODEL_AI_THINKER
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
 
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
 
-const int timerInterval = 30000;    // time between each HTTP POST image
-unsigned long previousMillis = 0;   // last time image was sent
+const int timerInterval = 30000;  // time between each HTTP POST image
+unsigned long previousMillis = 0; // last time image was sent
 
-void setup() {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
+void setup()
+{
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
   Serial.begin(115200);
 
   WiFi.mode(WIFI_STA);
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  WiFi.begin(ssid, password);  
-  while (WiFi.status() != WL_CONNECTED) {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(500);
   }
@@ -78,107 +98,208 @@ void setup() {
   config.pixel_format = PIXFORMAT_JPEG;
 
   // init with high specs to pre-allocate larger buffers
-  if(psramFound()){
+  if (psramFound())
+  {
     config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 10;  //0-63 lower number means higher quality
+    config.jpeg_quality = 10; // 0-63 lower number means higher quality
     config.fb_count = 2;
-  } else {
+  }
+  else
+  {
     config.frame_size = FRAMESIZE_CIF;
-    config.jpeg_quality = 12;  //0-63 lower number means higher quality
+    config.jpeg_quality = 12; // 0-63 lower number means higher quality
     config.fb_count = 1;
   }
-  
+
   // camera init
   esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
+  if (err != ESP_OK)
+  {
     Serial.printf("Camera init failed with error 0x%x", err);
     delay(1000);
     ESP.restart();
   }
 
-  sendPhoto(); 
+  sendPhoto();
 }
 
-void loop() {
+void loop()
+{
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= timerInterval) {
-    sendPhoto();
+  if (currentMillis - previousMillis >= timerInterval)
+  {
+    //sendPhoto();
+    take_send_photo();
     previousMillis = currentMillis;
   }
 }
 
-String sendPhoto() {
+String sendPhoto()
+{
   String getAll;
   String getBody;
 
-  camera_fb_t * fb = NULL;
+  camera_fb_t *fb = NULL;
   fb = esp_camera_fb_get();
-  if(!fb) {
+  if (!fb)
+  {
     Serial.println("Camera capture failed");
     delay(1000);
     ESP.restart();
   }
-  
+
   Serial.println("Connecting to server: " + serverName);
 
-  if (!client.connect(serverName.c_str(), serverPort)) {
-    Serial.println("Connection successful!");    
+  if (!client.connect(serverName.c_str(), serverPort))
+  {
+    Serial.println("Connection successful!");
     String head = "--attendance\r\nContent-Disposition: form-data; name=\"imageFile\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
     String tail = "\r\n--attendance--\r\n";
 
     uint32_t imageLen = fb->len;
     uint32_t extraLen = head.length() + tail.length();
     uint32_t totalLen = imageLen + extraLen;
-  
+
     client.println("POST " + serverPath + " HTTP/1.1");
     client.println("Host: " + serverName);
     client.println("Content-Length: " + String(totalLen));
     client.println("Content-Type: multipart/form-data; boundary=attendance");
     client.println();
     client.print(head);
-  
+
     uint8_t *fbBuf = fb->buf;
     size_t fbLen = fb->len;
-    for (size_t n=0; n<fbLen; n=n+1024) {
-      if (n+1024 < fbLen) {
+    for (size_t n = 0; n < fbLen; n = n + 1024)
+    {
+      if (n + 1024 < fbLen)
+      {
         client.write(fbBuf, 1024);
         fbBuf += 1024;
       }
-      else if (fbLen%1024>0) {
-        size_t remainder = fbLen%1024;
+      else if (fbLen % 1024 > 0)
+      {
+        size_t remainder = fbLen % 1024;
         client.write(fbBuf, remainder);
       }
-    }   
+    }
     client.print(tail);
-    
+
     esp_camera_fb_return(fb);
-    
+
     int timoutTimer = 10000;
     long startTimer = millis();
     boolean state = false;
-    
-    while ((startTimer + timoutTimer) > millis()) {
+
+    while ((startTimer + timoutTimer) > millis())
+    {
       Serial.print(".");
-      delay(100);      
-      while (client.available()) {
+      delay(100);
+      while (client.available())
+      {
         char c = client.read();
-        if (c == '\n') {
-          if (getAll.length()==0) { state=true; }
+        if (c == '\n')
+        {
+          if (getAll.length() == 0)
+          {
+            state = true;
+          }
           getAll = "";
         }
-        else if (c != '\r') { getAll += String(c); }
-        if (state==true) { getBody += String(c); }
+        else if (c != '\r')
+        {
+          getAll += String(c);
+        }
+        if (state == true)
+        {
+          getBody += String(c);
+        }
         startTimer = millis();
       }
-      if (getBody.length()>0) { break; }
+      if (getBody.length() > 0)
+      {
+        break;
+      }
     }
     Serial.println();
     client.stop();
     Serial.println(getBody);
   }
-  else {
-    getBody = "Connection to " + serverName +  " failed.";
+  else
+  {
+    getBody = "Connection to " + serverName + " failed.";
     Serial.println(getBody);
   }
   return getBody;
+}
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+  switch (evt->event_id) {
+    case HTTP_EVENT_ERROR:
+      Serial.println("HTTP_EVENT_ERROR");
+      break;
+    case HTTP_EVENT_ON_CONNECTED:
+      Serial.println("HTTP_EVENT_ON_CONNECTED");
+      break;
+    case HTTP_EVENT_HEADER_SENT:
+      Serial.println("HTTP_EVENT_HEADER_SENT");
+      break;
+    case HTTP_EVENT_ON_HEADER:
+      Serial.println();
+      Serial.printf("HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+      break;
+    case HTTP_EVENT_ON_DATA:
+      Serial.println();
+      Serial.printf("HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+      if (!esp_http_client_is_chunked_response(evt->client)) {
+        // Write out data
+        // printf("%.*s", evt->data_len, (char*)evt->data);
+      }
+      break;
+    case HTTP_EVENT_ON_FINISH:
+      Serial.println("");
+      Serial.println("HTTP_EVENT_ON_FINISH");
+      break;
+    case HTTP_EVENT_DISCONNECTED:
+      Serial.println("HTTP_EVENT_DISCONNECTED");
+      break;
+  }
+  return ESP_OK;
+}
+
+static esp_err_t take_send_photo()
+{
+  Serial.println("Taking picture...");
+  camera_fb_t *fb = NULL;
+  esp_err_t res = ESP_OK;
+
+  fb = esp_camera_fb_get();
+  if (!fb)
+  {
+    Serial.println("Camera capture failed");
+    return ESP_FAIL;
+  }
+
+  esp_http_client_handle_t http_client;
+
+  esp_http_client_config_t config_client = {0};
+  config_client.url = post_url;
+  config_client.event_handler = _http_event_handler;
+  config_client.method = HTTP_METHOD_POST;
+
+  http_client = esp_http_client_init(&config_client);
+
+  esp_http_client_set_post_field(http_client, (const char *)fb->buf, fb->len);
+
+  esp_http_client_set_header(http_client, "Content-Type", "image/jpg");
+
+  esp_err_t err = esp_http_client_perform(http_client);
+  if (err == ESP_OK)
+  {
+    Serial.print("esp_http_client_get_status_code: ");
+    Serial.println(esp_http_client_get_status_code(http_client));
+  }
+
+  esp_http_client_cleanup(http_client);
+
+  esp_camera_fb_return(fb);
 }
